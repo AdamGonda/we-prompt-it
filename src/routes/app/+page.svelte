7c385881521e7257dc781babd6c27477a67bcbdc
adamgonda <adamgondagyula@gmail.com>
2023-05-15 14:56:50 +0200

    refactor

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,24 +1,10 @@
 <script>
 	import { page } from '$app/stores';
-	import Repo from '$lib/components/repo.svelte';
-	
-
-	const { allRepos } = $page.data;
 </script>
 
-<h2>Logged in landing</h2>
-
 <main>
-	{#each allRepos as repo (repo.id) }
-		<Repo {repo}/>
-	{/each}
-
+	Auth landing
 </main>
 
 <style>
-
-	main {
-		display: flex;
-		flex-direction: column;
-	}
 </style>
--- src/routes/app/my-collection/+page.svelte
+++ src/routes/app/my-collection/+page.svelte
@@ -1,9 +1,23 @@
 <script>
 	import { page } from '$app/stores';
+	import Repo from '$lib/components/repo.svelte';
+	
 
+	const { allRepos } = $page.data;
 </script>
 
-<h2>My collection</h2>
+<main>
+	<h1>My collection</h1>
+	{#each allRepos as repo (repo.id) }
+		<Repo {repo}/>
+	{/each}
+
+</main>
 
 <style>
+
+	main {
+		display: flex;
+		flex-direction: column;
+	}
 </style>
