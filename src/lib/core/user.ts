e444496ccfd39c7d95044027404e860ad32f1954
adamgonda <adamgondagyula@gmail.com>
2023-05-15 19:00:02 +0200

    starts fixed

--- src/lib/core/user.ts
+++ src/lib/core/user.ts
@@ -1,58 +1,59 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export async function createUser(data) {
-  return await prisma.user.create({
-    data,
-  })
+	return await prisma.user.create({
+		data
+	});
 }
 
 export async function getUserByEmail(email) {
-  return await prisma.user.findUnique({
-    where: { email },
-  })
+	return await prisma.user.findUnique({
+		where: { email }
+	});
 }
 
 export async function getDBUser(event) {
-  const userEmail = (await event.locals.getSession()).user.email;
-  return await getUserByEmail(userEmail)
+	const userEmail = (await event.locals.getSession()).user.email;
+	return await getUserByEmail(userEmail);
 }
 
 export async function getUsersCollection(event) {
-  const dbUser = await getDBUser(event)
+	const dbUser = await getDBUser(event);
 
-  const createdBy = await prisma.repo.findMany({
-    where: {
-      authorId: dbUser.id
-    }
-  })
+	const createdBy = await prisma.repo.findMany({
+		where: {
+			authorId: dbUser.id
+		},
+		include: { stars: { where: { isDeleted: false } } }
+	});
 
-  const forked = createdBy.filter(repo => repo.isForked)
+	const forked = createdBy.filter((repo) => repo.isForked);
 
-  const stars = await prisma.star.findMany({
-    where: {
-      userId: dbUser.id,
-      isDeleted: false,
-    },
-    include: { repo: true }
-  })
-  
-  return {
-    forked,
-    createdBy,
-    starred: stars?.map(star => star.repo),
-  }
+	const stars = await prisma.star.findMany({
+		where: {
+			userId: dbUser.id,
+			isDeleted: false
+		},
+		include: { repo: { include: { stars: { where: { isDeleted: false } } } } }
+	});
+
+	return {
+		forked,
+		createdBy,
+		starred: stars?.map((star) => star.repo)
+	};
 }
 
 export async function updateUser(id, data) {
-  return await prisma.user.update({
-    where: { id },
-    data,
-  })
+	return await prisma.user.update({
+		where: { id },
+		data
+	});
 }
 
 export async function deleteUser(id) {
-  return await prisma.user.delete({
-    where: { id },
-  })
+	return await prisma.user.delete({
+		where: { id }
+	});
 }
