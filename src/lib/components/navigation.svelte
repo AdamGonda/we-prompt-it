d1a360774e2462253084736a29a665de5a747262
adamgonda <adamgondagyula@gmail.com>
2023-05-15 09:51:54 +0200

    refact

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -14,11 +14,11 @@
 		</a>
 
 		<!-- Prompt -->
-		<a href={`/app/prompt/1`}>
-			<div>Prompt</div>
-		</a>
 		<a href={`/app/prompt/create`}>
 			<div>Create prompt</div>
+		</a>
+		<a href={`/app/prompt/1`}>
+			<div>Prompt</div>
 		</a>
 		<a href={`/app/prompt/1/edit`}>
 			<div>Edit prompt</div>
