1965cacd47d4f52f01356794f3d1ab86f7264b0d
adamgonda <adamgondagyula@gmail.com>
2023-05-16 06:43:25 +0200

    update styles, refact

--- src/routes/app/explore/+page.svelte
+++ src/routes/app/explore/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	import { page } from '$app/stores';
 	import Repo from '$lib/components/repo.svelte';
-	
 
 	const { allRepos } = $page.data;
 </script>
@@ -11,11 +10,10 @@
 
 	<h3>All prompts</h3>
 	<div class="all">
-	{#each allRepos as repo (repo.id) }
-		<Repo {repo}/>
-	{/each}
+		{#each allRepos as repo (repo.id)}
+			<Repo {repo} />
+		{/each}
 	</div>
-
 </main>
 
 <style>
--- src/routes/app/my-collection/+page.svelte
+++ src/routes/app/my-collection/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	import { page } from '$app/stores';
 	import Repo from '$lib/components/repo.svelte';
-	
 
 	const { myCollection } = $page.data;
 </script>
@@ -10,25 +9,33 @@
 	<h1>My collection</h1>
 
 	<h3>Created by me</h3>
-	{#each myCollection.createdBy as repo (repo.id) }
-		<Repo {repo}/>
-	{/each}
+	<div class="all">
+		{#each myCollection.createdBy as repo (repo.id)}
+			<Repo {repo} />
+		{/each}
+	</div>
 
 	<h3>Forked</h3>
-	{#each myCollection.forked as repo (repo.id) }
-		<Repo {repo}/>
-	{/each}
-
+	<div class="all">
+		{#each myCollection.forked as repo (repo.id)}
+			<Repo {repo} />
+		{/each}
+	</div>
 
 	<h3>Starred</h3>
-	{#each myCollection.starred as repo (repo.id) }
-		<Repo {repo}/>
-	{/each}
-
+	<div class="all">
+		{#each myCollection.starred as repo (repo.id)}
+			<Repo {repo} />
+		{/each}
+	</div>
 </main>
 
 <style>
-
+	.all {
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		gap: 16px;
+	}
 	main {
 		display: flex;
 		flex-direction: column;
