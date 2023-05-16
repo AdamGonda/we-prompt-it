86763c762d467f52f980a51cd38977c2fd0c3a1e
adamgonda <adamgondagyula@gmail.com>
2023-05-16 10:03:08 +0200

    save query in store

--- src/lib/components/search-field.svelte
+++ src/lib/components/search-field.svelte
@@ -1,28 +1,28 @@
 <script>
-  export let onSearch = (query) => null
-  export let placeholder = 'search'
-  let query = '';
+	export let onSearch;
+	export let placeholder = 'search';
+	export let query = '';
 
-  let timeoutRef = null
-  $: delayedAction(query, 500)
+	let timeoutRef = null;
+	$: delayedAction(query, 500);
 
-  function delayedAction(query, delay) {
-    clearTimeout(timeoutRef)
+	function delayedAction(query, delay) {
+		clearTimeout(timeoutRef);
 
-    timeoutRef = setTimeout(() => {
-      if(query) onSearch(query)
-    }, delay)
-  }
+		timeoutRef = setTimeout(() => {
+			if (query) onSearch(query);
+		}, delay);
+	}
 </script>
 
-<input type="text" placeholder={placeholder} bind:value={query}>
+<input type="text" {placeholder} bind:value={query} />
 
 <style>
-  input {
-    width: 100%;
-    padding: 0.5rem;
-    border: 3px solid #ccc;
-    border-radius: 0.25rem;
-    font-size: 1.5rem;
-  }
+	input {
+		width: 100%;
+		padding: 0.5rem;
+		border: 3px solid #ccc;
+		border-radius: 0.25rem;
+		font-size: 1.5rem;
+	}
 </style>
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,28 +1,31 @@
 <script>
 	import { page } from '$app/stores';
-	import { results } from '$lib/stores';
+	import { query, results } from '$lib/stores/search';
 	import SearchField from '$lib/components/search-field.svelte';
 	import RepoCard from '$lib/components/repo-card.svelte';
 
-	async function onSearch(query) {
+	async function onSearch(q) {
 		const r = await fetch('/api/search', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			},
-			body: JSON.stringify({ query })
+			body: JSON.stringify({ query: q })
 		});
 
 		const newValue = await r.json();
 		results.update((value) => (value = newValue));
+		query.update((value) => (value = q));
 	}
+
+
 
 	const searchPlaceholder =
 		'Search prompts by name, description, content, tags, or AI model.';
 </script>
 
 <main>
-	<SearchField {onSearch} placeholder={searchPlaceholder} />
+	<SearchField query={$query} {onSearch} placeholder={searchPlaceholder}/>
 
 	<div class="all">
 		{#each $results as repo (repo.id)}
