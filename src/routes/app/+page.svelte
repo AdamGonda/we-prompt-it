05d785b789741522f49f8e83bf1f6e4b2d8790be
adamgonda <adamgondagyula@gmail.com>
2023-05-16 15:35:54 +0200

    refactor

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,25 +1,4 @@
 <script>
-	import { page } from '$app/stores';
-	import { query, results } from '$lib/stores/search';
-	import SearchField from '$lib/components/search-field.svelte';
-	import RepoCard from '$lib/components/repo-card.svelte';
-
-	async function onSearch(q) {
-		isLoading = true;
-		const r = await fetch('/api/search', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ query: q })
-		});
-
-		const newValue = await r.json();
-		results.update((value) => (value = newValue));
-		query.update((value) => (value = q));
-		isLoading = false;
-	}
-
 	const searchPlaceholder =
 		'Search prompts by name, description, content, tags, or AI model.';
 	let isLoading = false;
@@ -28,12 +7,4 @@
 Auth landing
 
 <style>
-	.all {
-		display: grid;
-		grid-template-columns: repeat(5, 1fr);
-		gap: 16px;
-	}
-	main {
-		padding: 1rem;
-	}
 </style>
