251bb9d1f27aedfa51cb2f5ff34605a4e00f8ff5
adamgonda <adamgondagyula@gmail.com>
2023-05-19 18:10:51 +0200

    update user in features

--- src/lib/features/user.ts
+++ src/lib/features/user.ts
@@ -1,12 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-export async function createUser(data) {
-	return await prisma.user.create({
-		data
-	});
-}
-
 export async function getUserByEmail(email) {
 	return await prisma.user.findUnique({
 		where: { email }
@@ -16,17 +10,4 @@ export async function getUserByEmail(email) {
 export async function getDBUser(event) {
 	const userEmail = (await event.locals.getSession()).user.email;
 	return await getUserByEmail(userEmail);
-}
-
-export async function updateUser(id, data) {
-	return await prisma.user.update({
-		where: { id },
-		data
-	});
-}
-
-export async function deleteUser(id) {
-	return await prisma.user.delete({
-		where: { id }
-	});
 }
