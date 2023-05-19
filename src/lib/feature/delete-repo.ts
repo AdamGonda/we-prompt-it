51d5bd5c3ea3b01cd4bfbccad115b9442ade7a26
adamgonda <adamgondagyula@gmail.com>
2023-05-19 15:31:30 +0200

    remove cnosole.log

--- src/lib/feature/delete-repo.ts
+++ src/lib/feature/delete-repo.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function deleteRepo(id) {
-  console.log('log id', id)
 	await prisma.repo.updateMany({
 		where: { parentId: id },
 		data: { parentId: null }
