8acb8e1eaa6b6358fb7cc4873d80befeffee81c4
adamgonda <adamgondagyula@gmail.com>
2023-05-21 18:28:17 +0200

    update login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,5 +2,19 @@
     <title>Login | We Prompt</title> 
 </svelte:head>
 
+<script>
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+    import { signIn, signOut } from '@auth/sveltekit/client';
+
+    const user = $page.data.session?.user;
+
+    if(browser && user) {
+        goto('/')
+    }
+</script>
+
 <!-- open ai style -->
-Login
+
+<button style="cursor: pointer;" on:click={() => signIn("google")} data-testid="login">Continue with google</button>
