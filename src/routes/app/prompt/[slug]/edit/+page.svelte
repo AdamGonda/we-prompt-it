ad213efdda42cf3e54f098e1c73b65c6b20ed629
adamgonda <adamgondagyula@gmail.com>
2023-05-31 15:29:14 +0200

    w

--- src/routes/app/prompt/[slug]/edit/+page.svelte
+++ src/routes/app/prompt/[slug]/edit/+page.svelte
@@ -5,11 +5,18 @@
 	import PromptForm from '$lib/components/prompt-form.svelte';
 	import routes from '$lib/routes';
 	import _ from 'lodash';
+	import { onMount } from 'svelte';
 
 	let confirmEditDialog;
 	let confirmDiscardDialog;
 	let confirmDeleteDialog;
 	let form;
+
+	onMount(() => {
+		// confirmEditDialog.showModal();
+		// confirmDiscardDialog.showModal();
+		confirmDeleteDialog.showModal();
+	});
 
 	function onDelete() {
 		return async () => {
@@ -53,31 +60,90 @@
 	>
 </PromptForm>
 
-<button
-	on:click={() => {
-		confirmDeleteDialog.showModal();
-	}}>Delete prompt</button
->
+<div class="danger-zone">
+	<p>Danger zone</p>
+	<button
+		on:click={() => {
+			confirmDeleteDialog.showModal();
+		}}>Delete</button
+	>
+</div>
 
-<dialog bind:this={confirmEditDialog}>
+<dialog class="confirm-edit" bind:this={confirmEditDialog}>
 	<button on:click={() => confirmEditDialog.close()}>Close</button>
 	<h2>Are you sure you want to edit?</h2>
 	<button on:click={form.requestSubmit()}>Submit</button>
 </dialog>
 
-<dialog bind:this={confirmDiscardDialog}>
+<dialog class="confirm-discard" bind:this={confirmDiscardDialog}>
 	<button on:click={() => confirmDiscardDialog.close()}>Close</button>
 	<h2>Are you sure you want to discard?</h2>
 	<button on:click={() => goto(routes.prompt(true, $page.params.slug))}>Submit</button>
 </dialog>
 
-<dialog bind:this={confirmDeleteDialog}>
-	<button on:click={() => confirmDeleteDialog.close()}>Close</button>
+<dialog class="confirm-delete" bind:this={confirmDeleteDialog}>
 	<form method="POST" action="?/delete" use:enhance={onDelete}>
-		Danger zone
-		<input type="submit" value="Delete" />
+		<p>Are you sure you want to proceed?</p>
+		<div>
+			<button type="button" on:click={() => confirmDeleteDialog.close()}>Cancel</button>
+			<input class="bubble" type="submit" value="Delete" />
+		</div>
 	</form>
 </dialog>
 
 <style>
+	.danger-zone {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: var(--s-5);
+	}
+
+	.danger-zone p {
+		border: 4px dashed red;
+		padding: var(--s-2) var(--s-4);
+		font-weight: 500;
+	}
+
+	dialog {
+		background: white;
+		border: none;
+		border-radius: var(--br-1);
+		padding: var(--s-6);
+	}
+
+	dialog::backdrop {
+		background-color: rgba(0, 0, 0, 0.5);
+	}
+
+	.confirm-delete form {
+		font-size: var(--fs-4);
+		display: flex;
+		flex-direction: column;
+		gap: var(--s-4);
+		align-items: center;
+	}
+
+	.confirm-delete p {
+		margin-top: 0;
+	}
+
+	.confirm-delete button,
+	input {
+		font-size: var(--fs-2);
+		background: none;
+		border: none;
+		cursor: pointer;
+	}
+
+	.confirm-delete input {
+		background: rgb(255, 38, 38);
+		color: white;
+		font-weight: bold;
+		margin-left: var(--s-4);
+	}
+
+	.confirm-delete input:hover {
+		background: rgb(211, 31, 31);
+	}
 </style>
