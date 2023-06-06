8d053fa5ef7f5262406aeae41c96cff51bf3b5e7
adamgonda <adamgondagyula@gmail.com>
2023-06-06 21:16:20 +0200

    add privacy policy and +page for it

---
+++ src/routes/privacy-policy/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+  <script src="https://app.termageddon.com/js/termageddon.js"></script>
+</svelte:head>
+
+<div
+	id="policy"
+	width="640"
+	height="480"
+	data-policy-key="ZEdGT1QzTnRjazlTT0doWU1FRTlQUT09"
+	data-extra="h-align=left&table-style=accordion"
+>
+	Please wait while the policy is loaded. If it does not load, please <a
+		rel="nofollow"
+		href="https://app.termageddon.com/api/policy/ZEdGT1QzTnRjazlTT0doWU1FRTlQUT09?h-align=left&table-style=accordion"
+		target="_blank">click here</a
+	>.
+</div>
