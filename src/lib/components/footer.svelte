60fa22845f6942a16773877ab0296610a19e0875
adamgonda <adamgondagyula@gmail.com>
2023-06-12 13:25:29 +0200

    update footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,8 +1,9 @@
 <footer class="footer">
 	<div>
-    <a href="/terms-of-service">Terms of service</a>
+		<a href="/terms-of-service">Terms of service</a>
 		<a href="/privacy-policy">Privacy policy</a>
 	</div>
+	<p class="open-beta">Open beta</p>
 </footer>
 
 <style>
@@ -10,11 +11,13 @@
 		color: var(--white);
 		background-color: var(--black);
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		flex-wrap: wrap;
     padding: var(--s-6) 0;
 		margin-top: var(--s-5);
+		position: relative;
 	}
 
   div {
@@ -26,5 +29,14 @@
 	a {
 		color: var(--white);
     text-decoration: none;
+		
+	}
+
+	.open-beta {
+		margin: 0;
+		margin-top: var(--s-5);
+		font-size: var(--fs-1);
+		border: 1PX solid var(--white);
+		padding: var(--s-1) var(--s-2);
 	}
 </style>
