d40824857467ce33cec2de9c23ff330238f3008e
adamgonda <adamgondagyula@gmail.com>
2023-06-12 16:01:36 +0200

    update privacy policy

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -1,16 +1,497 @@
 <svelte:head>
 	<title>Privacy policy | We Prompt</title>
-  <script src="https://app.termageddon.com/js/termageddon.js"></script>
+	<script src="https://app.termageddon.com/js/termageddon.js"></script>
 </svelte:head>
 
-<div
-	id="policy"
-	data-policy-key="ZEdGT1QzTnRjazlTT0doWU1FRTlQUT09"
-	data-extra="h-align=left&table-style=accordion"
->
-	Please wait while the policy is loaded. If it does not load, please <a
-		rel="nofollow"
-		href="https://app.termageddon.com/api/policy/ZEdGT1QzTnRjazlTT0doWU1FRTlQUT09?h-align=left&table-style=accordion"
-		target="_blank">click here</a
-	>.
+<h1>Privacy Policy</h1>
+<p class="tg-last-updated-date">Last updated: Jun 11, 2023 2:37 AM</p>
+<p>
+	We value your privacy very highly. Please read this Privacy Policy carefully before
+	using the weprompt Application (the "Application") operated by Adam Gonda ("us," "we,"
+	"our") as this Privacy Policy contains important information regarding your privacy and
+	how we may use the information we collect about you.
+</p>
+
+<p>
+	Your access to and use of the Application is conditional upon your acceptance of and
+	compliance with this Privacy Policy. This Privacy Policy applies to everyone, including,
+	but not limited to: visitors, users, and others, who wish to access or use the
+	Application.
+</p>
+
+<p>
+	By accessing or using the Application, you agree to be bound by this Privacy Policy. If
+	you disagree with any part of the Privacy Policy, then you do not have our permission to
+	access or use the Application.
+</p>
+
+<h2 class="">
+	What information we collect, how we use it, what happens if we don't have it, and the
+	legal basis for processing this information
+</h2>
+<p>
+	We collect any and all information that you enter on this Application. We collect the
+	following information about you:
+</p>
+<div class="accordions">
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">Identifying information - IP address</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Specific piece of information</h4>
+				<div class="accordion-section-content">IP address</div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">
+					Legal basis for processing this information
+				</h4>
+				<div class="accordion-section-content">
+					<ul>
+						<li>The user has provided consent to the processing of their information.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">How we use this information</h4>
+				<div class="accordion-section-content"><ul><li>Analytics.</li></ul></div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">
+					What happens if we don't have this information
+				</h4>
+				<div class="accordion-section-content">
+					<ul><li>Our use of your data for analytics will not be possible.</li></ul>
+				</div>
+			</div>
+		</div>
+	</details>
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">Identifying information - Email address</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Specific piece of information</h4>
+				<div class="accordion-section-content">Email address</div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">
+					Legal basis for processing this information
+				</h4>
+				<div class="accordion-section-content">
+					<ul>
+						<li>The user has provided consent to the processing of their information.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">How we use this information</h4>
+				<div class="accordion-section-content"><ul><li>Analytics.</li></ul></div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">
+					What happens if we don't have this information
+				</h4>
+				<div class="accordion-section-content">
+					<ul><li>Our use of your data for analytics will not be possible.</li></ul>
+				</div>
+			</div>
+		</div>
+	</details>
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">
+				Identifying information - Device identifier
+			</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Specific piece of information</h4>
+				<div class="accordion-section-content">Device identifier</div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">
+					Legal basis for processing this information
+				</h4>
+				<div class="accordion-section-content">
+					<ul>
+						<li>The user has provided consent to the processing of their information.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">How we use this information</h4>
+				<div class="accordion-section-content"><ul><li>Analytics.</li></ul></div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">
+					What happens if we don't have this information
+				</h4>
+				<div class="accordion-section-content">
+					<ul><li>Our use of your data for analytics will not be possible.</li></ul>
+				</div>
+			</div>
+		</div>
+	</details>
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">
+				Internet or other electronic activity - Browsing history
+			</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Specific piece of information</h4>
+				<div class="accordion-section-content">Browsing history</div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">
+					Legal basis for processing this information
+				</h4>
+				<div class="accordion-section-content">
+					<ul>
+						<li>The user has provided consent to the processing of their information.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">How we use this information</h4>
+				<div class="accordion-section-content"><ul><li>Analytics.</li></ul></div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">
+					What happens if we don't have this information
+				</h4>
+				<div class="accordion-section-content">
+					<ul><li>Our use of your data for analytics will not be possible.</li></ul>
+				</div>
+			</div>
+		</div>
+	</details>
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">
+				Internet or other electronic activity - Search history
+			</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Specific piece of information</h4>
+				<div class="accordion-section-content">Search history</div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">
+					Legal basis for processing this information
+				</h4>
+				<div class="accordion-section-content">
+					<ul>
+						<li>The user has provided consent to the processing of their information.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">How we use this information</h4>
+				<div class="accordion-section-content"><ul><li>Analytics.</li></ul></div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">
+					What happens if we don't have this information
+				</h4>
+				<div class="accordion-section-content">
+					<ul><li>Our use of your data for analytics will not be possible.</li></ul>
+				</div>
+			</div>
+		</div>
+	</details>
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">
+				Internet or other electronic activity - Information regarding your interaction
+				with our website or application
+			</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Specific piece of information</h4>
+				<div class="accordion-section-content">
+					Information regarding your interaction with our website or application
+				</div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">
+					Legal basis for processing this information
+				</h4>
+				<div class="accordion-section-content">
+					<ul>
+						<li>The user has provided consent to the processing of their information.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">How we use this information</h4>
+				<div class="accordion-section-content"><ul><li>Analytics.</li></ul></div>
+			</div>
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">
+					What happens if we don't have this information
+				</h4>
+				<div class="accordion-section-content">
+					<ul><li>Our use of your data for analytics will not be possible.</li></ul>
+				</div>
+			</div>
+		</div>
+	</details>
 </div>
+<h2 class="">With whom we share your personal information</h2>
+<p>We do not share your personal information with any third parties.</p>
+
+<h2 class="">Cookies</h2>
+<p>
+	A cookie is a small piece of data sent from a application and stored on your device by
+	your browser. This Application collects cookies and may use cookies for reasons
+	including, but not limited to:
+</p>
+<ul>
+	<li>Analyzing our Application traffic;</li>
+	<li>Analyzing your interactions with advertisements;</li>
+	<li>Identifying if you are signed in to the Application;</li>
+	<li>Testing content on the Application;</li>
+	<li>Storing information about your preferences;</li>
+	<li>Recognizing when you return to the Application.</li>
+</ul>
+
+<p>
+	Most web browsers automatically accept cookies. However, you can modify your browser
+	settings to decline cookies, if you prefer. This setting may prevent you from taking
+	full advantage of the Application.
+</p>
+
+<h2 class="">Children's privacy</h2>
+
+<p>
+	This Application is intended for use by a general audience and does not offer services
+	to children. Should a child whom we know to be under 18 send personal information to us,
+	we will use that information only to respond to that child to inform him or her that
+	they cannot use this Application.
+</p>
+
+<h2 class="">Analytics programs</h2>
+
+<p>
+	This Application uses Google Analytics to collect information about you and your
+	behaviors. If you would like to opt out of Google Analytics, please visit <a
+		href="https://tools.google.com/dlpage/gaoptout/"
+		target="_blank">https://tools.google.com/dlpage/gaoptout/</a
+	>.
+</p>
+
+<h2 class="">Information retention</h2>
+<p>
+	We retain all of the information that we collect until we no longer need the information
+</p>
+
+<h2 class="">Your rights</h2>
+<p>
+	Depending upon where you reside, you may have the following rights with regard to your
+	personal information:
+</p>
+<div class="accordions">
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">
+				The right to access the personal information that we have collected about you.
+			</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Applies To</h4>
+				<div class="accordion-section-content">
+					Residents of the European Union and/or the European Economic Area and the United
+					Kingdom only
+				</div>
+			</div>
+		</div>
+	</details>
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">
+				The right to request that we delete all or some of the personal information that
+				we have collected on you.
+			</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Applies To</h4>
+				<div class="accordion-section-content">
+					Residents of the European Union and/or the European Economic Area and the United
+					Kingdom only
+				</div>
+			</div>
+		</div>
+	</details>
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">
+				The right to ask us to transmit your personal information that we have collected
+				on you to another provider (where technically feasible).
+			</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Applies To</h4>
+				<div class="accordion-section-content">
+					Residents of the European Union and/or the European Economic Area and the United
+					Kingdom only
+				</div>
+			</div>
+		</div>
+	</details>
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">
+				The right to request that we amend any of the information that we have collected
+				about you.
+			</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Applies To</h4>
+				<div class="accordion-section-content">
+					Residents of the European Union and/or the European Economic Area and the United
+					Kingdom only
+				</div>
+			</div>
+		</div>
+	</details>
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">
+				The right to withdraw your consent to the processing of your data.
+			</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Applies To</h4>
+				<div class="accordion-section-content">
+					Residents of the European Union and/or the European Economic Area and the United
+					Kingdom only
+				</div>
+			</div>
+		</div>
+	</details>
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">
+				The right to request that we restrict the processing of your data.
+			</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Applies To</h4>
+				<div class="accordion-section-content">
+					Residents of the European Union and/or the European Economic Area and the United
+					Kingdom only
+				</div>
+			</div>
+		</div>
+	</details>
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">
+				The right to lodge a complaint regarding our collection, sharing and processing of
+				data with competent authorities in the proper jurisdiction.
+			</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Applies To</h4>
+				<div class="accordion-section-content">
+					Residents of the European Union and/or the European Economic Area and the United
+					Kingdom only
+				</div>
+			</div>
+		</div>
+	</details>
+	<details class="accordion">
+		<summary class="accordion-summary"
+			><h3 class="accordion-heading">
+				The right to stop receiving unwanted direct marketing.
+			</h3></summary
+		>
+		<div class="accordion-content">
+			<div class="accordion-section">
+				<h4 class="accordion-section-heading">Applies To</h4>
+				<div class="accordion-section-content">
+					Residents of the European Union and/or the European Economic Area only
+				</div>
+			</div>
+		</div>
+	</details>
+</div>
+<h2 class="">Exercising your rights</h2>
+<p>You may exercise the rights specified above by submitting a consumer request to:</p>
+<p>
+	Adam Gonda<br />CEO<br />support@weprompt.it<br />+36703598875<br />weprompt.it<br
+	/>2030 Erd Somogyvari u 2.<br />Hungary<br />
+</p>
+<p>
+	We will respond to most consumer requests within 30 to 45 days of receipt, depending
+	upon where you reside. However, some requests may take longer. We will notify you in
+	writing if we need more time to respond. We have the ability to deny your request(s) if
+	certain exceptions in the law apply. If we do deny your request, we will provide you
+	with the reasons for such denial.
+</p>
+
+<h2 class="">Location of data processing</h2>
+<p>All data processing activities undertaken by us take place in Hungary.</p>
+
+<h2 class="">Third-party websites</h2>
+
+<p>
+	This Application may contain hyperlinks to websites operated by parties other than us.
+	We provide such hyperlinks for your reference only. We do not control such websites and
+	are not responsible for their contents or the privacy or other practices of such
+	websites. It is up to you to read and fully understand their Privacy Policies. Our
+	inclusion of hyperlinks to such websites does not imply any endorsement of the material
+	on such websites or any association with their operators.
+</p>
+
+<h2 class="">Questions</h2>
+<p>
+	If you have any questions about this Privacy Policy, please contact us at
+	hello@weprompt.it.
+</p>
+
+<style>
+	.accordions {
+		border: 1px solid #aaa;
+		border-radius: 4px;
+		margin: 16px 0;
+	}
+
+	.accordion {
+		border-bottom: 1px solid #aaa;
+	}
+	
+	.accordion-summary {
+		cursor: pointer;
+		padding: 0.5em 0.5em;
+	}
+
+	.accordion-heading {
+		display: inline;
+		margin: 0;
+	}
+
+	.accordion-content {
+		margin: 0;
+		padding: 0.5em 1.5em;
+		display: unset !important;
+	}
+
+	.accordion-section-heading {
+		margin: 0;
+	}
+</style>
