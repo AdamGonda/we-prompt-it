3689dc07ba58a0e0b0565a98ca157494bfb908e4
adamgonda <adamgondagyula@gmail.com>
2023-06-15 16:28:07 +0200

    u description

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -2,4 +2,4 @@ export const url = 'https://we-prompt-it.vercel.app';
 export const title = 'Explore, Collect, and Create AI Prompts | We Prompt';
 export const image = `${url}/og-image.jpg`;
 export const description =
-	'We are an open, community-driven platform where everyone, from AI enthusiasts to professionals, can find the perfect prompt to solve any problem. Dive into our expansive collection of prompts to explore, collect, and create. Our platform fosters an environment of collaboration and continual improvement, all underpinned by the unique feature of accessible prompt engineering for all.';
+	'We are an open, community-driven platform where everyone, from AI enthusiasts to prompt engineers, can find the perfect prompt to solve any problem. Dive into our expansive collection of prompts to explore, collect, and create.';
